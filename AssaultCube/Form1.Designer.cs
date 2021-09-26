f827d19ea5f841f7f8097caf9d5abeb4931e8d13
Koda <naus5450@hotmail.com>
2021-09-26 09:30:41 -0400

    Single and multiple offset ammo support.

--- AssaultCube/Form1.Designer.cs
+++ AssaultCube/Form1.Designer.cs
@@ -29,13 +29,58 @@ namespace AssaultCube
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ammoTxtBox = new System.Windows.Forms.TextBox();
+            this.ammoButton = new System.Windows.Forms.Button();
+            this.attachButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ammoTxtBox
+            // 
+            this.ammoTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ammoTxtBox.Location = new System.Drawing.Point(12, 47);
+            this.ammoTxtBox.Name = "ammoTxtBox";
+            this.ammoTxtBox.Size = new System.Drawing.Size(155, 20);
+            this.ammoTxtBox.TabIndex = 5;
+            // 
+            // ammoButton
+            // 
+            this.ammoButton.Location = new System.Drawing.Point(173, 47);
+            this.ammoButton.Name = "ammoButton";
+            this.ammoButton.Size = new System.Drawing.Size(107, 20);
+            this.ammoButton.TabIndex = 4;
+            this.ammoButton.Text = "Set MTP-57 Ammo";
+            this.ammoButton.UseVisualStyleBackColor = true;
+            // 
+            // attachButton
+            // 
+            this.attachButton.Location = new System.Drawing.Point(12, 12);
+            this.attachButton.Name = "attachButton";
+            this.attachButton.Size = new System.Drawing.Size(268, 29);
+            this.attachButton.TabIndex = 3;
+            this.attachButton.Text = "Attach";
+            this.attachButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(294, 78);
+            this.Controls.Add(this.ammoTxtBox);
+            this.Controls.Add(this.ammoButton);
+            this.Controls.Add(this.attachButton);
+            this.Name = "Form1";
+            this.Text = "Assault Cube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ammoTxtBox;
+        private System.Windows.Forms.Button ammoButton;
+        private System.Windows.Forms.Button attachButton;
     }
 }
 
--- AssaultCube/Form1.cs
+++ AssaultCube/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace AssaultCube
@@ -16,5 +10,81 @@ namespace AssaultCube
         {
             InitializeComponent();
         }
+        public Process gameProc;
+        public Int32 gamePID;
+        public IntPtr hProc;
+        public IntPtr baseAddress = IntPtr.Zero;
+        public ProcessModule mainModule;
+        public void attachGame()
+        {
+            Process[] processesByName = Process.GetProcessesByName("ac_client");
+            if (processesByName.Length < 1)
+            {
+                MessageBox.Show("Game is not running!");
+                return;
+            }
+            gameProc = processesByName[0];
+            gamePID = gameProc.Id;
+            hProc = functions.OpenProcess(0x1F0FFF, bInheritHandle: false, gameProc.Id);
+            mainModule = gameProc.MainModule;
+            baseAddress = mainModule.BaseAddress;
+            MessageBox.Show("Connected");
+            ammoButton.Enabled = true;
+            ammoTxtBox.Enabled = true;
+        }
+        private void attachButton_Click(object sender, EventArgs e)
+        {
+            attachGame();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ammoButton.Enabled = false;
+            ammoTxtBox.Enabled = false;
+        }
+        private void ammoButton_Click(object sender, EventArgs e)
+        {
+            setAmmo();
+        }
+        public void setAmmo()
+        {
+            IntPtr lpNumberOfBytesWritten; //need a blank variable to store output of writeprocessmemory
+
+            byte[] memoryAddress = new byte[4]; //create a new byte with size depending on required bytes (normally 4 or 8)
+
+            functions.ReadProcessMemory(hProc, (UIntPtr)((int)mainModule.BaseAddress + 0x0010F4F4), memoryAddress, 4L, IntPtr.Zero); //read processmemory to get memoryAddress
+
+            uint ptr = BitConverter.ToUInt32(memoryAddress, 0); //convert bytes to uint32
+            MessageBox.Show(ptr.ToString());
+            functions.WriteProcessMemory(hProc, (IntPtr)ptr + 336, Convert.ToInt32(ammoTxtBox.Text), 4L, out lpNumberOfBytesWritten); //write to converted memoryAddress adding offset
+
+            //Get 336 from 150 as HEX -> convert 150 hex to 336 decimal
+
+            MessageBox.Show("Ammo Set!");
+        }
+
+        public void setAmmoMultiOffset()
+        {
+            //asdjifsdhkulsdflkhjdfslhjkfdglhjkudfgkjhldfgjkhfgdhjklfgdhljkdfghjlkdgfjkhgdf
+            IntPtr lpNumberOfBytesWritten; //need a blank variable to store output of writeprocessmemory
+
+            byte[] memoryAddress = new byte[4]; //create a new byte with size depending on required bytes (normally 4 or 8)
+
+            functions.ReadProcessMemory(hProc, (UIntPtr)((int)mainModule.BaseAddress + 0x0010F418), memoryAddress, 4L, IntPtr.Zero); //read processmemory to get memoryAddress
+            //
+            //
+            uint ptr = BitConverter.ToUInt32(memoryAddress, 0); //convert bytes to uint32 to get our pointer
+            functions.ReadProcessMemory(hProc, (UIntPtr)((int)ptr + 24), memoryAddress, 4L, IntPtr.Zero); //read processmemory at this new address to get our next pointer
+
+            ptr = BitConverter.ToUInt32(memoryAddress, 0); //store our new pointer in the old var
+
+
+            functions.WriteProcessMemory(hProc, (IntPtr)ptr + 336, Convert.ToInt32(ammoTxtBox.Text), 4L, out lpNumberOfBytesWritten); //write to converted memoryAddress adding final offset
+
+            //Get 336 from 150 as HEX -> convert 150 hex to 336 decimal
+
+            MessageBox.Show("Ammo Set!");
+
+        }
+
     }
 }
